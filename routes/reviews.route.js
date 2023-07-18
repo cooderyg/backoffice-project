b104482301d9a60fa6a46930f5320dddacdf79a7
Kim Jinkyung <jinkyung127@naver.com>
2023-07-18 14:51:01 +0900

    리뷰기능 수정

--- routes/reviews.route.js
+++ routes/reviews.route.js
@@ -14,13 +14,15 @@ router.post('/orders/:orderId/reviews', async (req, res) => {
       comment: comment,
       rating: rate,
     });
-    res.status(200).json({ message: '리뷰를 생성하였습니다.' });
+    if (!comment) {
+      return res.status(400).json({ message: '리뷰 내용을 입력해주세요.' });
+    } else {
+      res.status(200).json({ message: '리뷰를 생성하였습니다.' });
+    }
   } catch (error) {
     res.status(400).json({ errorMessage: error.message });
   }
 });
-
-// 리뷰 사진 첨부
 
 // 리뷰 목록 조회
 router.get('/users/:userId/reviews', async (req, res) => {
@@ -47,7 +49,12 @@ router.get('/users/:userId/reviews', async (req, res) => {
       order: [['createdAt', 'DESC']],
     });
 
-    return res.status(200).json({ data: reviews });
+    if (!reviews) {
+      return res.status(400).json({ message: '작성한 리뷰가 없습니다.' });
+    }
+    if (reviews) {
+      return res.status(200).json({ data: reviews });
+    }
   } catch (error) {
     res.status(400).json({ errorMessage: error.message });
   }
@@ -66,8 +73,12 @@ router.put('/users/:userId/reviews/:reviewId', async (req, res) => {
       return res.status(400).json({ message: '존재하지 않는 리뷰입니다.' });
     }
     if (review) {
-      await reviews.update({ rating, comment }, { where: { reviewId: reviewId } });
-      res.status(201).json({ message: '리뷰가 정상적으로 수정되었습니다.' });
+      if (!comment) {
+        return res.status(412).json({ message: '리뷰 내용을 입력해주세요.' });
+      } else {
+        await reviews.update({ rating, comment }, { where: { reviewId: reviewId } });
+        res.status(201).json({ message: '리뷰가 정상적으로 수정되었습니다.' });
+      }
     }
   } catch (error) {
     res.status(400).json({ errorMessage: error.message });
