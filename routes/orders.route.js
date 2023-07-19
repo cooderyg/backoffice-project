498a0c083a67101a662bf35b505e9f7aa31bfba1
yerim <sola4718@gmail.com>
2023-07-19 20:40:58 +0900

    주문서 CRUD 완료

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const {} = require('../models');
+const {
+  createOrder,
+  getOrder,
+  updateOrder,
+  deleteOrder,
+} = require('../controllers/orders.controller');
+const authMiddleware = require('../middlewares/validations/signup.validation.js');
+
+// 주문 생성 API
+router.post('/orders', authMiddleware, createOrder);
+
+// 주문서 상세 조회 API
+router.get('/:userId/orders', getOrder);
+
+// 주문서 수정 API
+router.put('/:userId/orders/:orderId', authMiddleware, updateOrder);
+
+// 주문서 삭제 API
+router.delete('/:userId/orders/:orderId', authMiddleware, deleteOrder);
 
 module.exports = router;
