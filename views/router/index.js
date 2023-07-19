8d2a2717ac42192448c38295f4f4c1b0c0729636
cooderyg <ygk9604@naver.com>
2023-07-19 17:57:12 +0900

    [추가] detail 페이지 & 쿠키 장바구니추가

--- views/router/index.js
+++ views/router/index.js
@@ -12,4 +12,8 @@ router.get('/payment', (req, res) => {
 router.get('/login', (req, res) => {
   return res.render('login');
 });
+
+router.get('/detail', (req, res) => {
+  return res.render('detail');
+});
 module.exports = router;
