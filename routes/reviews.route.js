38c09e5e3152f1425514f90bacd2c533497b02e3
Kim Jinkyung <jinkyung127@naver.com>
2023-07-19 21:43:46 +0900

    ㄹ

--- routes/reviews.route.js
+++ routes/reviews.route.js
@@ -5,9 +5,9 @@ const authMiddleware = require('../middlewares/auth-middleware');
 const ReviewsController = require('../controllers/reviews.controller');
 const reviewsController = new ReviewsController();
 
-router.post('/orders/:orderId/reviews', reviewsController.createReview);
-router.get('/users/:userId/reviews', reviewsController.getReviews);
-router.put('/users/:userId/reviews/:reviewId', reviewsController.updateReview);
-router.delete('/users/:userId/reviews/:reviewId', reviewsController.deleteReview);
+router.post('/orders/:orderId/reviews', authMiddleware, reviewsController.createReview);
+router.get('/users/:userId/reviews', authMiddleware, reviewsController.getReviews);
+router.put('/users/:userId/reviews/:reviewId', authMiddleware, reviewsController.updateReview);
+router.delete('/users/:userId/reviews/:reviewId', authMiddleware, reviewsController.deleteReview);
 
 module.exports = router;
