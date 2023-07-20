63734791167afd8cb8ceda79540e85a85fb4eeaa
cooderyg <ygk9604@naver.com>
2023-07-21 08:59:50 +0900

    [추가] 주문완료 페이지, socketIO 보일러 플레이트

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ const reviewsRouter = require('./routes/reviews.route.js');
 const storesRouter = require('./routes/stores.route.js');
 const filesRouter = require('./routes/files.route.js');
 const viewRouter = require('./views/router');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const PORT = 3000;
-
+const server = http.createServer(app);
+const io = new Server(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -31,10 +34,35 @@ app.use('/api', [
   storesRouter,
 ]);
 
+let users = [];
+io.use((socket, next) => {
+  cookieParser()(socket.request, socket.request.res || {}, next);
+});
+io.on('connection', async (socket) => {
+  const req = socket.request;
+  const accessToken = req.accessToken;
+  if (!accessToken) next(new Error('미로그인입니다.'));
+  try {
+    const decoded = jwt.verify(req.cookies.user, process.env.ACCESS_KEY);
+    decoded.userId
+      ? (socket.data.userId = decoded.userId)
+      : (socket.data.ownerId = decoded.ownerId);
+  } catch {
+    next(new Error('유효하지 않은 토큰입니다.'));
+  }
+
+  users.push(socket);
+
+  socket.on('disconnect', () => {
+    users.splice(users.indexOf(socket), 1);
+  });
+  next();
+});
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/views/static'));
 app.use('/', viewRouter);
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
 });
