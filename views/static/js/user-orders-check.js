bed245a86d2b1f4ec191065e5d51dc82ffd17891
Kim Jinkyung <jinkyung127@naver.com>
2023-07-20 20:08:46 +0900

    리뷰조회페이지로 이동

--- views/static/js/user-orders-check.js
+++ views/static/js/user-orders-check.js
@@ -2,10 +2,14 @@
 const reviewButtons = document.querySelectorAll('.reviewBtn');
 reviewButtons.forEach((button) => {
   button.addEventListener('click', function () {
-    // 리뷰 작성 페이지로 이동하는 URL
-    const reviewWriteURL = '/reviewWrite';
+    window.location.href = '/reviewWrite';
+  });
+});
 
-    // 리뷰 작성 페이지로 이동
-    window.location.href = reviewWriteURL;
+// 리뷰 조회 버튼 클릭 시 리뷰 조회 페이지로 이동
+const reviewListButtons = document.querySelectorAll('.reviewListBtn');
+reviewListButtons.forEach((button) => {
+  button.addEventListener('click', function () {
+    window.location.href = '/reviewList';
   });
 });
