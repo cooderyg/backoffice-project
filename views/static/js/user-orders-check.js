7e3533efb0ab77478909071b670aad0c297a6aff
Kim Jinkyung <jinkyung127@naver.com>
2023-07-21 15:21:48 +0900

    d

--- views/static/js/user-orders-check.js
+++ views/static/js/user-orders-check.js
@@ -1,3 +1,32 @@
+const parseCookie = (cookie) => {
+  return cookie
+    .split(';')
+    .map((value) => {
+      return value.split('=');
+    })
+    .reduce((acc, el) => {
+      acc[decodeURIComponent(el[0].trim())] = decodeURIComponent(el[1].trim());
+      return acc;
+    }, {});
+};
+
+// 주문 내역 불러오기
+window.addEventListener('DOMContentLoaded', async function () {
+  const { accessToken } = parseCookie(document.cookie);
+
+  const data = await fetch('/api/currentUser', {
+    headers: {
+      cookie: accessToken,
+    },
+  });
+  const {
+    user: { userId },
+  } = await data.json();
+
+  fetch(`/${userId}/orders`).then((response) => response.json());
+  console.log(response);
+});
+
 // 리뷰 쓰기 버튼 클릭 시 리뷰 작성 페이지로 이동
 const reviewButtons = document.querySelectorAll('.reviewBtn');
 reviewButtons.forEach((button) => {
